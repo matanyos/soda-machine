eb31be0a58d259139fa8da104eb2517b8e8c599f
matanyos <37904698+matanyos@users.noreply.github.com>
2022-05-12 13:09:35 +0200

    updated request DTO

--- SodaMachine/Models/Request.cs
+++ SodaMachine/Models/Request.cs
@@ -2,14 +2,47 @@
 
 namespace SodaMachine.Models;
 
-public class Request
+public abstract class Request
 {
-    public Request(RequestType type, string input)
+    public sealed class InsertMoneyRequest : Request
     {
-        Type = type;
-        Input = input;
-    }
+        public InsertMoneyRequest(RequestType type, decimal money)
+        {
+            Type = type;
+            Money = money;
+        }
 
-    public RequestType Type { get; }
-    public string Input { get; }
+        public RequestType Type { get; }
+        public decimal Money { get; }
+    }
+    public sealed class OrderRequest : Request
+    {
+        public OrderRequest(RequestType type, SodaType sodaType)
+        {
+            Type = type;
+            SodaType = sodaType;
+        }
+
+        public RequestType Type { get; }
+        public SodaType SodaType { get; }
+    }
+    public sealed class SmsOrderRequest : Request
+    {
+        public SmsOrderRequest(RequestType type, SodaType sodaType)
+        {
+            Type = type;
+            SodaType = sodaType;
+        }
+        public RequestType Type { get; }
+        public SodaType SodaType { get; }
+    }
+    public sealed class RecallRequest : Request
+    {
+        public RecallRequest(RequestType type)
+        {
+            Type = type;
+        }
+
+        public RequestType Type { get; }
+    }
 }
